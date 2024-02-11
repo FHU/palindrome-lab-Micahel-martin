05f793505b5dceed1b502abfd92f32a89c720b2c
Micahel-martin <michael.martin@students.fhu.edu>
2024-02-10 21:17:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def palindrome(word):
-    word.palindrome == True
-return 'False'
+def palindrome(a_string):
+        reversed_string = ''
+        for i in range(len(a_string), 0, -1):
+              reversed_string += a_string[i-1]
+              return a_string == reversed_string
 
 #YOUR CODE GOES HERE
